c0d59a500c95d2543af8075b1e5d6e19ed4a824c
Yixuan Zhang <janicezhang.me@gmail.com>
2016-10-22 16:16:27 +0000

    Janice finished the 1st version of assignment 6,dataset 2 and 3 are the same

--- 7.1/script.js
+++ 7.1/script.js
@@ -18,9 +18,9 @@ var scaleX, scaleY;
 
 //Step 1: importing multiple datasets
 d3.queue()
-    //.defer()
-    //.defer()
-    //.defer()
+    .defer(d3.csv,'../data/olympic_medal_count_1900.csv',parse)
+    .defer(d3.csv,'../data/olympic_medal_count_1960.csv',parse)
+    .defer(d3.csv,'../data/olympic_medal_count_2012.csv',parse)
     .await(function(err,rows1900,rows1960,rows2012){
         //Draw axis
         scaleY = d3.scaleLinear()
@@ -39,9 +39,17 @@ d3.queue()
             .attr('transform','translate(-100,0)')
             .call(axisY);
 
+        draw(rows2012);
+        
         //Step 2: implement the code to switch between three datasets
         d3.select('#year-1900').on('click', function(){
             draw(rows1900);
+        });
+        d3.select('#year-1960').on('click', function(){
+            draw(rows1960);
+        });
+        d3.select('#year-2012').on('click', function(){
+            draw(rows2012);
         });
 
     });
@@ -49,10 +57,62 @@ d3.queue()
 //Step 3: implement the enter / exit / update pattern
 function draw(rows){
     var top5 = rows.sort(function(a,b){
-        return b.count2012 - a.count2012;
+        return b.count - a.count;
     }).slice(0,5);
-
+    
     console.log(top5);
+    //Represent bar chart
+    var countries = plot.selectAll('.country')
+        .data(top5,function(d){return d.country});//update
+
+    //Enter
+    var countriesEnter = countries.enter()
+        .append('g')
+        .attr('class','country')
+        .attr('transform',function(d,i){
+            return 'translate('+scaleX(i)+',0)';
+        });
+        
+    countriesEnter.append('rect')
+        .attr('y',function(d){
+            return scaleY(d.count);
+        })
+        .attr('width',40)
+        .attr('height',function(d){
+            return h-scaleY(d.count);
+        });
+    countriesEnter.append('text')
+        .attr('y', h+20)
+        .text(function(d){
+            return d.country;
+        });
+        
+    //Exit
+    countries.exit()
+        .transition()
+        .duration(1000)
+        .style('fill-opacity',0.5)
+        .attr('y',0)
+        .attr('height', 0)
+        .remove();
+        
+    //Update
+    var countriesTransition = countries
+        .merge(countriesEnter)
+        .transition()
+        .duration(1000)
+        
+        .attr('transform',function(d,i){
+            return 'translate('+scaleX(i)+',0)';
+        });
+        
+    countriesTransition.select('rect')
+        .attr('y',function(d){
+            return scaleY(d.count);
+        })
+        .attr('height',function(d){
+            return h-scaleY(d.count);
+        });
 }
 
 function parse(d){
